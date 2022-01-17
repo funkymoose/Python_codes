d40317d41dd38d4ef516cdd86cc1c33d591f1983
Somesh Patil <67376135+funkymoose@users.noreply.github.com>
2022-01-18 01:29:03 +0530

    Prob1

---
+++ TCS Codevita/Prob1.py
@@ -0,0 +1,40 @@
+'''
+Question – :  Some prime numbers can be expressed as a sum of other consecutive prime numbers.
+
+For example
+5 = 2 + 3,
+17 = 2 + 3 + 5 + 7,
+41 = 2 + 3 + 5 + 7 + 11 + 13.
+Your task is to find out how many prime numbers which satisfy this property are present in the range 3 to N subject to a constraint that summation should always start with number 2.
+Write code to find out the number of prime numbers that satisfy the above-mentioned property in a given range.
+
+Input Format: First line contains a number N
+
+Output Format: Print the total number of all such prime numbers which are less than or equal to N.
+
+Constraints: 2<N<=12,000,000,000
+'''
+
+def factor(n):
+    count = 0
+    for i in range(2,n):
+        if n%i==0:
+            count+=1
+    if count==0:
+        return True
+    return False
+def total(num):
+    l=[i for i in range(2,num+1) if factor(i)]
+    count = 0
+    for n in range(len(l)):
+        i = 0
+        x = 0
+        while i<n:
+            x+=l[i]
+            if x == l[n]:
+                count+=1
+                break
+            else:
+                i+=1
+    return count
+print(total(50))
